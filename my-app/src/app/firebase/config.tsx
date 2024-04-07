cac7880914829e5ecb17348209a2062af24d3371
yohan manohara <warushayohan80@gmail.com>
2024-04-08 01:16:31 +0530

    make googgle atuntication

--- my-app/src/app/firebase/config.tsx
+++ my-app/src/app/firebase/config.tsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC5fbz6ZcMm4NxVcpbscdjP4SHfFjKlKSg",
-    authDomain: "centering-star-405502.firebaseapp.com",
-    projectId: "centering-star-405502",
-    storageBucket: "centering-star-405502.appspot.com",
-    messagingSenderId: "157740077766",
-    appId: "1:157740077766:web:af53267aa26dcabba7fdc7",
-    measurementId: "G-4KB6B4SMM1"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId:process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- my-app/src/app/signin/page.tsx
+++ my-app/src/app/signin/page.tsx
@@ -19,6 +19,7 @@ import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
 import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
 import {  signInWithEmailAndPassword} from 'firebase/auth';
 import { auth } from '@/app/firebase/config'
+import { GoogleAuthProvider,signInWithPopup } from "firebase/auth";
 
 
 
@@ -142,7 +143,30 @@ export default function signin() {
                 </Typography>
               </Stack>
               <Button
-              
+              onClick={() => {
+                      const provider = new GoogleAuthProvider();
+                      signInWithPopup(auth, provider)
+                        .then((result) => {
+                          // This gives you a Google Access Token. You can use it to access the Google API.
+                          const credential = GoogleAuthProvider.credentialFromResult(result);
+                          const token = credential?.accessToken;
+                          // The signed-in user info.
+                          const user = result.user;
+                          // IdP data available using getAdditionalUserInfo(result)
+                          // ...
+                        }).catch((error) => {
+                          // Handle Errors here.
+                          const errorCode = error.code;
+                          const errorMessage = error.message;
+                          // The email of the user's account used.
+                          const email = error.customData.email;
+                          // The AuthCredential type that was used.
+                          const credential = GoogleAuthProvider.credentialFromError(error);
+                          // ...
+                        });
+
+              }}
+             
               >
                 Continue with Google
               </Button>
